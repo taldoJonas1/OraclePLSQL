723db238e52b56770db9889404bcb2fd2792cd65
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2024-08-20 20:47:34 -0300

    Update loja.sql

--- loja.sql
+++ loja.sql
@@ -160,3 +160,86 @@ VALUES (1, NULL, 'James', 'Smith', 'CEO', 8000.00, 1);
 
 INSERT INTO tb_funcionarios(id_funcionario, id_gerente, nome, sobrenome, cargo, salario, fg_ativo)
 VALUES (2, 1, 'Ron', 'Johnson', 'Sales Manager', 6000.00, 1);
+
+-- Data (20/08/2024)
+
+INSERT INTO tb_grades_salarios(id_salario, base_salario, teto_salario, fg_ativo)
+VALUES
+(1, 1, 2500.00, 1);
+
+INSERT INTO tb_grades_salarios(id_salario, base_salario, teto_salario, fg_ativo)
+VALUES
+(2, 2500.01, 5000.00, 1);
+
+INSERT INTO tb_grades_salarios(id_salario, base_salario, teto_salario, fg_ativo)
+VALUES
+(3, 5000.01, 7500.00, 1);
+
+INSERT INTO tb_grades_salarios(id_salario, base_salario, teto_salario, fg_ativo)
+VALUES
+(4, 7500.01, 9999.99, 1);
+
+COMMIT;
+
+
+SELECT *
+FROM tb_grades_salarios;
+
+
+UPDATE tb_clientes
+  SET sobrenome = 'Orange'
+WHERE id_cliente = 2;
+
+SELECT *
+FROM tb_clientes;
+
+ROLLBACK;
+
+
+DELETE
+FROM tb_clientes
+WHERE id_cliente = 2;
+-- RA-02292: restrição de integridade (LOJA.FK_TB_COMPRAS_ID_CLIENTE) violada - registro filho localizado
+
+
+CREATE OR REPLACE PROCEDURE get_cliente(p_id_cliente IN tb_clientes.id_cliente%TYPE)
+AS
+ v_nome         tb_clientes.nome%TYPE;
+ v_sobrenome    tb_clientes.sobrenome%TYPE;
+ v_controle     INTEGER;
+BEGIN
+ SELECT COUNT(1) INTO v_controle
+ FROM tb_clientes
+ WHERE id_cliente = p_id_cliente;
+ 
+ 	IF (v_controle = 1) THEN
+   SELECT nome, sobrenome INTO v_nome, v_sobrenome
+   FROM tb_clientes
+   WHERE id_cliente = p_id_cliente;
+   
+   DBMS_OUTPUT.PUT_LINE('Cliente localizado: ' || v_nome || ' ' || v_sobrenome);
+ ELSE
+   DBMS_OUTPUT.PUT_LINE('Cliente NÃO localizado');   
+ END IF;
+ 
+EXCEPTION 
+  WHEN others THEN
+    DBMS_OUTPUT.PUT_LINE('Erro!!!'); 
+END get_cliente;
+
+-- Invocando o objeto "get_cliente" por meio da saída DBMS OUTPUT
+-- Menu: Exibir > Saída Dbms
+CALL get_cliente(1);
+
+CALL get_cliente(90);
+
+-- Invocando o objeto "get_cliente" por meio de um bloco anônimo
+SET serveroutput ON
+BEGIN
+  get_cliente(3);
+END;
+
+SET serveroutput ON
+BEGIN
+  get_cliente(99);
+END;
